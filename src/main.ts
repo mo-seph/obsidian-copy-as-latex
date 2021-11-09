55c83264668d10209de131cf9f6375146335d51d
mo-seph <github@mo-seph.com>
2021-11-09 13:02:25 +0100

    Moved conversion functions into top-level globals

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,7 @@ import { Literal } from 'mdast';
 const electron = require('electron')
 const clipboard = electron.clipboard;
 
+/* No settings provided yet... */
 interface CopyAsLatexPluginSettings {
 	mySetting: string;
 }
@@ -22,11 +23,12 @@ const DEFAULT_SETTINGS: CopyAsLatexPluginSettings = {
 }
 
 
+
 export default class CopyAsLatexPlugin extends Plugin {
 	settings: CopyAsLatexPluginSettings; 
 
 	async onload() {
-		//console.log('loading Copy as Latex');
+		console.log('loading Copy as Latex');
 		await this.loadSettings();
 		this.addCommand({
 			id: 'copy-as-latex',
@@ -58,74 +60,90 @@ export default class CopyAsLatexPlugin extends Plugin {
 			mdastExtensions: [wikiLink.fromMarkdown(),gfmFromMarkdown()]
 		});
 		console.log(ast);
-		const result = this.ASTtoString(ast)
+		const result = ASTtoString(ast)
 		console.log(result)
-		clipboard.writeText(result)
+		navigator.clipboard.writeText(result)
 		return true;
 	}
 
-	ASTtoString(input:Node,indent:number=0) : string {
-		var t = input.type;
-		type Convert = (a:Node) => string
-		const defaultC : Convert = (a:Node) => {return (a as Literal).value};
-		const wrapper = (jn:string,aft:string,ind=0) => (a:Node) => {return (
-			(a as Parent).children.map((c) => this.ASTtoString(c,ind)).join(jn)) + aft};
-		const heading = (a:Node) => {
-			const h = a as Heading
-			var sec = "section"
-			if( h.depth == 2 ) sec = "subsection"
-			if( h.depth == 3 ) sec = "subsubsection"
-			return "\\" + sec + "{" + this.ASTtoString((a as Parent).children[0]) + "}\n"
-		}
 
-		const list = (a:Node) => {
-			const h = a as List
-			var sec = h.ordered ? "enumerate" : "itemize"
-			return "\t".repeat(indent) + "\\begin{" + sec + "}\n" + 
-				wrapper("","",indent+1)(a) +
-				"\t".repeat(indent) + "\\end{" + sec + "}\n"
-		}
-		const internalLink = (a:Node) => {
-			const h = a as wikiLink
-			const url:string = h.value
-			if(url.startsWith("@") ) { return "\\cite{" + url.substring(1) + "}" }
-			if(url.startsWith("^") ) { return "\\ref{" + url.substring(1) + "}" }
-			return url 
-		}
-		const externalLink = (a:Node) => {
-			const l = a as Link
-			console.log("Got a link!")
-			console.log(a)
-			return "\\url{" + l.url + "}"
-		}
-		
-		const code = (a:Node) => {
-			const cd = a as Code
-			return `\\begin{lstlisting}[language=${cd.lang}]
+}
+
+// Conversions go from a Node with a certain amount of indentation to a string
+type Convert = (a:Node,indent:number) => string
+
+/*
+ * Overall function to carry out the conversion
+ */
+function ASTtoString(input:Node,indent:number=0) : string {
+	var t = input.type;
+
+
+	const transforms : { [key:string] : Convert } = {
+		'root': wrapper("\n",""),
+		'paragraph': wrapper("","\n"),
+		'emphasis': (a:Node) => {return "\\emph{" + wrapper("","")(a) + "}"},
+		'strong': (a:Node) => {return "\\textbf{" + wrapper("","")(a) + "}"},
+		'delete': (a:Node) => {return "\\st{" + wrapper("","")(a) + "}"},
+		'footnote': (a:Node) => {return "\\footnote{" + wrapper("","")(a) + "}"},
+		'list' : list,
+		'listItem': (a:Node) => {return "\t".repeat(indent) + "\\item " + wrapper("","")(a,indent) },
+		'heading': heading,
+		'wikiLink': internalLink,
+		'link': externalLink,
+		'code': code
+	}
+	const f:Convert = transforms[input.type] || defaultC
+	const trans = f(input,indent)
+	return   trans
+}
+
+/*
+ * Individual functions to convert elements
+ */
+
+const defaultC : Convert = (a:Node,indent:number=0) => {return (a as Literal).value};
+const wrapper = (jn:string,aft:string) => (a:Node,indent:number=0) => {return (
+	(a as Parent).children.map((c) => ASTtoString(c,indent)).join(jn)) + aft};
+const heading = (a:Node,indent:number=0) => {
+	const h = a as Heading
+	var sec = "section"
+	if( h.depth == 2 ) sec = "subsection"
+	if( h.depth == 3 ) sec = "subsubsection"
+	return "\\" + sec + "{" + ASTtoString((a as Parent).children[0]) + "}\n"
+}
+
+const list = (a:Node,indent:number=0) => {
+	const h = a as List
+	var sec = h.ordered ? "enumerate" : "itemize"
+	return "\t".repeat(indent) + "\\begin{" + sec + "}\n" + 
+		wrapper("","")(a,indent+1) +
+		"\t".repeat(indent) + "\\end{" + sec + "}\n"
+}
+const internalLink = (a:Node,indent:number=0) => {
+	const h = a as wikiLink
+	const url:string = h.value
+	if(url.startsWith("@") ) { return "\\cite{" + url.substring(1) + "}" }
+	if(url.startsWith("^") ) { return "\\ref{" + url.substring(1) + "}" }
+	return url 
+}
+const externalLink = (a:Node,indent:number=0) => {
+	const l = a as Link
+	console.log("Got a link!")
+	console.log(a)
+	return "\\url{" + l.url + "}"
+}
+
+const code = (a:Node,indent:number=0) => {
+	const cd = a as Code
+	return `\\begin{lstlisting}[language=${cd.lang}]
 ${cd.value}
 \\end{lstlisting}
 `
-		}
-
-		const transforms : { [key:string] : Convert } = {
-			'root': wrapper("\n",""),
-			'paragraph': wrapper("","\n"),
-			'emphasis': (a:Node) => {return "\\emph{" + wrapper("","")(a) + "}"},
-			'strong': (a:Node) => {return "\\textbf{" + wrapper("","")(a) + "}"},
-			'delete': (a:Node) => {return "\\st{" + wrapper("","")(a) + "}"},
-			'footnote': (a:Node) => {return "\\footnote{" + wrapper("","")(a) + "}"},
-			'list' : list,
-			'listItem': (a:Node) => {return "\t".repeat(indent) + "\\item " + wrapper("","",indent)(a) },
-			'heading': heading,
-			'wikiLink': internalLink,
-			'link': externalLink,
-			'code': code
-		}
-		const f:Convert = transforms[input.type] || defaultC
-		const trans = f(input)
-		return   trans
-	}
 }
+
+
+
 
 /*
 class SampleSettingTab extends PluginSettingTab {
